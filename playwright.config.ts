31bef9ffe350b26783b317d37ebe7dc4a8ceedb2
unknown <nilesh.qa2012@gmail.com>
2025-08-31 10:48:03 +0530

    change playwright workflow file and headless true from config

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
-    headless: false,
+    headless: true,
     video: {
       mode: 'on',    },
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
